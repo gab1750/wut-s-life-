14a356b0b83abcf9beab1ecf90d5ae6aa4d4b953
Garrett Brown <gab1750@icloud.com>
2019-07-16 14:14:20 -0700

    Initial Commit

---
+++ wut's life?Tests/wut_s_life_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  wut_s_life_Tests.swift
+//  wut's life?Tests
+//
+//  Created by Garrett Brown on 7/16/19.
+//  Copyright © 2019 Garrett Brown. All rights reserved.
+//
+
+import XCTest
+@testable import wut_s_life_
+
+class wut_s_life_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
